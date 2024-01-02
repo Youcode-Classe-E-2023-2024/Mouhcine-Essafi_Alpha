357ed585493e330184fc46e184067ea73f256f66
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-02 15:27:06 +0100

    Create _layout.php

---
+++ views/_layout.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= ucfirst($page) ?></title>
+    <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <main>
+        <?php include_once 'views/' . $page . '_view.php'; ?>
+    </main>
+
+    <footer></footer>
+    <script src="<?= PATH ?>assets/js/main.js"></script>
+</body>
+</html>

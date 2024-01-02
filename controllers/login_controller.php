1ffca5ebf31a90a2c644d4fa81f95a512e4e505a
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-02 15:27:15 +0100

    Create login_controller.php

---
+++ controllers/login_controller.php
@@ -0,0 +1,17 @@
+<?php
+// session_start();
+
+if (isset($_POST['submit_l'])) {
+    $userRegistration = new UserLogin($db);
+    $registrationResult = $userRegistration->loginUser($_POST['log_email'], $_POST['log_password']);
+    // echo '<div class="alert">' . $registrationResult . '</div>';
+    if (is_int($registrationResult) === true) {
+        header('location: index.php?page=home');
+        $_SESSION['id'] = $registrationResult;
+    }
+    else{
+        header('location: index.php?page=login');
+    
+    }
+}
+?>

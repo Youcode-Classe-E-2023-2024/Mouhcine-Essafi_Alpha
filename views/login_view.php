72d392744d4facb3b272ed119e2ecee0b158a0ae
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-04 10:12:30 +0100

    Update login_view.php

--- views/login_view.php
+++ views/login_view.php
@@ -6,22 +6,23 @@
                     <div class="flex flex-col flex-1 justify-center mb-8">
                         <h1 class="text-4xl text-center font-thin">Welcome Back</h1>
                         <div class="w-full mt-4">
-                            <form class="form-horizontal w-3/4 mx-auto" action="" method="POST">
+                            <form class="form-horizontal w-3/4 mx-auto" id="loginForm">
                                 <div class="flex flex-col mt-4">
-                                    <input type="text" class="flex-grow h-8 px-2 border rounded border-grey-400" name="log_email" placeholder="Email">
+                                    <input type="text" class="flex-grow h-8 px-2 border rounded border-grey-400" id="username" name="log_email" placeholder="Email">
                                 </div>
                                 <div class="flex flex-col mt-4">
-                                    <input type="password" class="flex-grow h-8 px-2 rounded border border-grey-400" name="log_password" required placeholder="Password">
+                                    <input type="password" class="flex-grow h-8 px-2 rounded border border-grey-400" id="password" name="log_password" required placeholder="Password">
                                 </div>
                                 <div class="flex items-center mt-4">
                                     <input type="checkbox" name="remember" id="remember" class="mr-2"> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
                                 </div>
                                 <div class="flex flex-col mt-8">
-                                    <button name="submit_l" type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
+                                    <button id="submitBtn" name="submit_l" type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
                                         Login
                                     </button>
                                 </div>
                             </form>
+                            <div id="result"></div>
                             <div class="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
                                 Don't have an account? <a href="index.php?page=register" id="sign-up" class="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign up</a>
                             </div>
@@ -33,3 +34,4 @@
         </div>
     </div>
 </div>
+<!-- <script src="../assets/js/main.js"></script> -->
